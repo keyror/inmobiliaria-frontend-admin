d74f9a598ba0021af363bbecb4c2104c26c1b515
keyror <44626685+keymiro@users.noreply.github.com>
2026-04-15 09:52:30 -0500

    se ajusta version de zod y se realiza url dinamica para dominio en apiurl.ts

--- composables/apiurl.ts
+++ composables/apiurl.ts
@@ -1,1 +1,5 @@
-export const baseUrl = 'http://localhost:3000'
+const host = typeof window !== 'undefined' ? window.location.host : 'localhost:3000'
+
+export const baseUrl = host.includes('localhost')
+    ? 'http://localhost:3000'
+    : `${window.location.protocol}//${host}`
