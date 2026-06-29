2e0d6d36c5af5078daccb5e6b3490c00f4c60da6
keyror <44626685+keymiro@users.noreply.github.com>
2026-06-28 23:14:17 -0500

    se ajusta plans

---
+++ app/pages/plans/all.vue
@@ -0,0 +1,14 @@
+<template>
+  <PlansAll />
+</template>
+<script setup lang="ts">
+definePageMeta({
+  permission: "plans.view",
+});
+
+useHead({
+  title: "Gestión de Planes",
+});
+</script>
+
+<style scoped></style>
